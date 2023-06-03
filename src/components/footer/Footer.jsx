0150b62b3463dbc34b628626259a55ca666f0c35
ThaiSonDev <sonthaidev91@gmail.com>
2023-06-03 12:17:12 +0700

    Nextjs footer image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className={styles.container}>© Copyright - All rights reserved.</div>
+    <div className={styles.container}>
+      <div>© Copyright - All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          className={styles.icon}
+          src="/1.png"
+          width={15}
+          height={15}
+          alt="thaisonDevFac"
+        />
+        <Image
+          className={styles.icon}
+          src="/2.png"
+          width={15}
+          height={15}
+          alt="thaisonDevFac"
+        />
+        <Image
+          className={styles.icon}
+          src="/3.png"
+          width={15}
+          height={15}
+          alt="thaisonDevFac"
+        />
+        <Image
+          className={styles.icon}
+          src="/4.png"
+          width={15}
+          height={15}
+          alt="thaisonDevFac"
+        />
+      </div>
+    </div>
   );
 };
 
